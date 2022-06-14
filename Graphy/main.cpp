d382a4c7091788659c2d012edb578330309b05df
rahul0tripathi <rahultripathidev@gmail.com>
2022-06-15 01:37:52 +0530

    Initial Commit

---
+++ Graphy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Graphy
+//
+//  Created by ATS on 15/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
